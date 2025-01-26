a90f9acd6f47181cb117fb6e8e2eaf1891b0796f
Oana Trinc <161495792+trinc-oana1@users.noreply.github.com>
2025-01-26 20:32:07 +0200

    Add files via upload

---
+++ Project_part2/project2.m
@@ -0,0 +1,17 @@
+miu = 0.25;
+U_in = 48;
+U_out = 12;
+frequency = 1500;
+R = 2.4;
+
+L_calculated = 0.006;
+C_calculated = 0.000104;
+Id_calculated = 3.75;
+It_max_calculated = 5.5;
+
+
+L_chosen = 0.006;
+C_chosen = 0.0001;
+Id_chosen = 5;
+It_max_chosen = 5.5;
+
---
+++ Project_part3/project3.m
@@ -0,0 +1,32 @@
+clc
+clear
+
+miu = 0.25;
+R = 2.4;
+L = 0.006;
+C = 0.000104;
+Id =3.75;
+It_max =5.5;
+U_in = 48;
+U_out = 12;
+frequency = 1500;
+
+sigma = 0.06;
+t_r = 0.007;
+zeta = (abs(log(sigma)))/sqrt(log(sigma)^2 + pi^2);
+
+w_n = 4/(t_r* zeta);
+
+A = [0 -1/L; 1/C -1/(R*C)];
+B = [U_in/L; 0];
+C = [0 1];
+D = 0;
+
+[nump, denp] = ss2tf(A, B, C, D);
+H_p = tf(nump, denp);
+
+H_o = tf([0, w_n^2], [1, 2*zeta*w_n, w_n^2]);
+H_c = 1/H_p * H_o/(1-H_o);
+% H_c = 1/H_p * feedback(H_o, 1);
+[numc, denc] = tfdata(H_c);
+
